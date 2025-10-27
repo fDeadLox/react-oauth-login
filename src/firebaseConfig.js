b93a9308c16d7d5649f64f8b8d4ec379dd9e9012
fDeadLox <brentvee28@gmail.com>
2025-10-27 16:45:16 +0800

    Update for API Key

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import {initializeApp} from 'firebase/app';
 import {getAuth, GoogleAuthProvider, GithubAuthProvider, EmailAuthProvider} from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyC800UsLJ3iRRrsWrDLNVqFaeLFSBXCHQo",
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "reactoauth-7143a.firebaseapp.com",
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "reactoauth-7143a",
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "reactoauth-7143a.firebasestorage.app",
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "275223138229",
-    appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:275223138229:web:f08a89dd880b18f54553fc",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
